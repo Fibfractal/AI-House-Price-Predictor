626123d2539f571c0e733ea9f301347d2376c827
Rob <robert.systemdeveloper@gmail.com>
2021-03-29 12:51:46 +0200

    Added pseudo code to About.vue

---
+++ frontend/src/views/About.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+      <h2>About the service</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo rem possimus necessitatibus quas vero similique adipisci? Consectetur harum rem dolore?</p>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+</style>

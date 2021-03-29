00c8c30622875b73ede110190c78e026d69475a8
Rob <robert.systemdeveloper@gmail.com>
2021-03-29 12:49:35 +0200

    Added the navbar html code

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,37 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-secondary bg-secondary justify-content-center p-4">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <router-link to='/'>Make a prediction</router-link>
+            </li>
+            <li class="nav-item">
+                <router-link to='/predictions'>Display predictions</router-link>
+            </li>
+            <li class="nav-item">
+                <router-link to='/about'>About</router-link>
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+    a {
+        margin: 10px;
+        text-decoration:none;
+        color: white;
+        font-size: 22px;
+    }
+
+    a:hover {
+        color: rgb(190, 100, 100);
+        cursor: pointer;
+        transition: 0.25s;
+    }
+</style>

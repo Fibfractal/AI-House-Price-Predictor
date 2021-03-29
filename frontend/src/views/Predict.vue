2e3cdf495aa2a87f21d514d744b4e122aed60e96
Rob <robert.systemdeveloper@gmail.com>
2021-03-29 13:23:20 +0200

    Seting up rows and cols with bootstrap grid

---
+++ frontend/src/views/Predict.vue
@@ -0,0 +1,33 @@
+<template>
+    <main class="container-fluid">
+        <div class="row">
+            <div class="col-12 col-md-4 p-4 data-input-col">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere unde repudiandae debitis impedit necessitatibus perspiciatis quaerat quibusdam sequi enim laudantium!</p>
+            </div>
+
+            <div class="col-12 col-md-8 p-4 display-prediction-col">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere unde repudiandae debitis impedit necessitatibus perspiciatis quaerat quibusdam sequi enim laudantium!</p>
+            </div>
+        </div>
+    </main>
+</template>
+
+<script>
+export default {
+  data(){
+
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<style scope>
+    .data-input-col {
+        background-color: rgb(199, 206, 204);
+    }
+    .display-prediction-col {
+        background-color: rgb(219, 211, 219);
+    }
+</style>

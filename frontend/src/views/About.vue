9e777bb2af873dd7fd4b1ede6ec91641d266ac62
Ulrika L <u.lifvenborg@outlook.com>
2021-03-31 13:11:24 +0200

    Test

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,13 +1,23 @@
 <template>
-  <div>
-      <h2>About the service</h2>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo rem possimus necessitatibus quas vero similique adipisci? Consectetur harum rem dolore?</p>
+   <div class="container">
+      <div class="row">
+          <div class="col-4">
+                
+          </div>
+          <div class="col-8">
+              <h2>About the service</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo rem possimus necessitatibus quas vero similique adipisci? Consectetur harum rem dolore?</p>
+          </div>
+      </div>
   </div>
 </template>
 
 <script>
-export default {
-}
+
+  export default {
+     
+  }
+  
 </script>
 
 <style>

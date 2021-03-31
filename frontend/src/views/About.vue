65018f3e2a8102947b9d2d981ee036765620cd2d
Ulrika L <u.lifvenborg@outlook.com>
2021-03-31 23:18:53 +0200

    Adde carousels and images in About.vue

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -2,6 +2,40 @@
    <div class="container">
       <div class="row">
           <div class="col-4">
+            <p></p>
+            <div id="carouselExampleSlidesOnly1" class="carousel slide" data-bs-ride="carousel" data-bs-interval=3000>
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img src="src\assets\jacques-bopp-wtzOhxEX4WU-unsplash-500-296.jpg" class="d-block w-100" alt="A braun, wood house">
+                </div>
+                <div class="carousel-item">
+                  <img src="src\assets\jacques-bopp-Hh18POSx5qk-unsplash-500-296.jpg" class="d-block w-100" alt="A white house with red roof">
+                </div>
+                <div class="carousel-item">
+                  <img src="src\assets\phil-hearing-IYfp2Ixe9nM-unsplash-500-296.jpg" class="d-block w-100" alt="A white house">
+                </div>
+                <div class="carousel-item">
+                  <img src="src\assets\roger-starnes-sr-0MA4WC4I2tw-unsplash-500-296.jpg" class="d-block w-100" alt="Another white house">
+                </div>
+              </div>
+            </div>
+            <p></p>
+            <div id="carouselExampleSlidesOnly2" class="carousel slide" data-bs-ride="carousel" data-bs-interval=2500>
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img src="src\assets\roger-starnes-sr-MINQCvKfwL4-unsplash-500-296.jpg" class="d-block w-100" alt="A blue house">
+                </div>
+                <div class="carousel-item">
+                  <img src="src\assets\roger-starnes-sr-nPmyejZsOy0-unsplash-500-296.jpg" class="d-block w-100" alt="A white house with a tall tree">
+                </div>
+                <div class="carousel-item">
+                  <img src="src\assets\roger-starnes-sr-WhYsgTb1LMQ-unsplash-500-296.jpg" class="d-block w-100" alt="A white house with a flag">
+                </div>
+                <div class="carousel-item">
+                  <img src="src\assets\todd-kent-178j8tJrNlc-unsplash-500-296.jpg" class="d-block w-100" alt="A house with a porch">
+                </div>
+              </div>
+            </div>
                 
           </div>
           <div class="col-8">

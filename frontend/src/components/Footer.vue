b36a3e5a79abd17c740e906d7fc9689b71337bb1
Rob <robert.systemdeveloper@gmail.com>
2021-04-01 09:36:20 +0200

    Created footer

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+    <footer class="container-fluid bg-secondary text-light p-3">
+        <div class="row">
+            <div class="col-12 col-md-4 text-center">&copy; 2021 Fast predictor</div>
+            <div class="col-12 col-md-4 text-center">predictor@fastpredictor.com</div>
+            <div class="col-12 col-md-4 text-center">010-123 456</div>
+        </div>
+  </footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>

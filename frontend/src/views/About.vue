7a08f2b11fd264dead607df842246a452dea2431
Rob <robert.systemdeveloper@gmail.com>
2021-04-03 12:22:53 +0200

    Grid position and look changes for about

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,49 +1,61 @@
 <template>
-   <div class="container">
+   <div class="container-fluid">
       <div class="row">
-          <div class="col-4">
-            <p></p>
-            <div id="carouselExampleSlidesOnly1" class="carousel slide" data-bs-ride="carousel" data-bs-interval=3000>
-              <div class="carousel-inner">
-                <div class="carousel-item active">
-                  <img src="src\assets\jacques-bopp-wtzOhxEX4WU-unsplash-500-296.jpg" class="d-block w-100" alt="A braun, wood house">
-                </div>
-                <div class="carousel-item">
-                  <img src="src\assets\jacques-bopp-Hh18POSx5qk-unsplash-500-296.jpg" class="d-block w-100" alt="A white house with red roof">
-                </div>
-                <div class="carousel-item">
-                  <img src="src\assets\phil-hearing-IYfp2Ixe9nM-unsplash-500-296.jpg" class="d-block w-100" alt="A white house">
-                </div>
-                <div class="carousel-item">
-                  <img src="src\assets\roger-starnes-sr-0MA4WC4I2tw-unsplash-500-296.jpg" class="d-block w-100" alt="Another white house">
-                </div>
-              </div>
+          <div class="col-12 col-md-4 p-4 left-lg-col">
+            <div class="row">
+                <div class="col-12 col-md-2"></div>
+                  <div class="col-12 col-md-8">
+                    <div id="carouselExampleSlidesOnly1" class="carousel slide" data-bs-ride="carousel" data-bs-interval=3000>
+                      <div class="carousel-inner">
+                        <div class="carousel-item active">
+                          <img src="src\assets\jacques-bopp-wtzOhxEX4WU-unsplash-500-296.jpg" class="d-block w-100" alt="A braun, wood house">
+                        </div>
+                        <div class="carousel-item">
+                          <img src="src\assets\jacques-bopp-Hh18POSx5qk-unsplash-500-296.jpg" class="d-block w-100" alt="A white house with red roof">
+                        </div>
+                        <div class="carousel-item">
+                          <img src="src\assets\phil-hearing-IYfp2Ixe9nM-unsplash-500-296.jpg" class="d-block w-100" alt="A white house">
+                        </div>
+                        <div class="carousel-item">
+                          <img src="src\assets\roger-starnes-sr-0MA4WC4I2tw-unsplash-500-296.jpg" class="d-block w-100" alt="Another white house">
+                        </div>
+                      </div>
+                    </div>
+                    <p></p>
+                    <div id="carouselExampleSlidesOnly2" class="carousel slide" data-bs-ride="carousel" data-bs-interval=2500>
+                      <div class="carousel-inner">
+                        <div class="carousel-item active">
+                          <img src="src\assets\roger-starnes-sr-MINQCvKfwL4-unsplash-500-296.jpg" class="d-block w-100" alt="A blue house">
+                        </div>
+                        <div class="carousel-item">
+                          <img src="src\assets\roger-starnes-sr-nPmyejZsOy0-unsplash-500-296.jpg" class="d-block w-100" alt="A white house with a tall tree">
+                        </div>
+                        <div class="carousel-item">
+                          <img src="src\assets\roger-starnes-sr-WhYsgTb1LMQ-unsplash-500-296.jpg" class="d-block w-100" alt="A white house with a flag">
+                        </div>
+                        <div class="carousel-item">
+                          <img src="src\assets\todd-kent-178j8tJrNlc-unsplash-500-296.jpg" class="d-block w-100" alt="A house with a porch">
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  </div>
+                  <div class="col-12 col-md-2"></div>
             </div>
-            <p></p>
-            <div id="carouselExampleSlidesOnly2" class="carousel slide" data-bs-ride="carousel" data-bs-interval=2500>
-              <div class="carousel-inner">
-                <div class="carousel-item active">
-                  <img src="src\assets\roger-starnes-sr-MINQCvKfwL4-unsplash-500-296.jpg" class="d-block w-100" alt="A blue house">
+          <div class="col-12 col-md-8 mt-4 p-4">
+              <div class="row">
+                <div class="col-12 col-md-1"></div>
+                <div class="col-12 col-md-10 ">
+                  <h4>About the service</h4>
+                  <p class = "input-info">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, unde, ratione quos eaque eos voluptatibus voluptate doloremque iure quasi neque aperiam, minima hic! Vel nisi eius pariatur eum vitae. Deleniti fuga illum numquam quisquam officiis, sapiente ipsum quasi fugiat obcaecati exercitationem delectus, alias excepturi tempore eveniet, saepe eum tempora impedit. Reprehenderit doloremque repellat laborum ullam incidunt, deleniti corrupti vel laboriosam, culpa accusantium distinctio adipisci nisi officiis animi earum vitae voluptates similique impedit? Non corporis eveniet at nemo minus recusandae perspiciatis repellendus est deleniti quod, numquam illum aliquid nobis saepe. Magnam repellendus deserunt nobis earum delectus saepe excepturi voluptas harum ea.
+                  </p>
                 </div>
-                <div class="carousel-item">
-                  <img src="src\assets\roger-starnes-sr-nPmyejZsOy0-unsplash-500-296.jpg" class="d-block w-100" alt="A white house with a tall tree">
-                </div>
-                <div class="carousel-item">
-                  <img src="src\assets\roger-starnes-sr-WhYsgTb1LMQ-unsplash-500-296.jpg" class="d-block w-100" alt="A white house with a flag">
-                </div>
-                <div class="carousel-item">
-                  <img src="src\assets\todd-kent-178j8tJrNlc-unsplash-500-296.jpg" class="d-block w-100" alt="A house with a porch">
-                </div>
-              </div>
+                <div class="col-12 col-md-1"></div>
             </div>
-                
-          </div>
-          <div class="col-8">
-              <h2>About the service</h2>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo rem possimus necessitatibus quas vero similique adipisci? Consectetur harum rem dolore?</p>
           </div>
       </div>
   </div>
+
 </template>
 
 <script>
@@ -54,5 +66,14 @@
   
 </script>
 
-<style>
+<style scope>
+  .input-info {
+    float: left;
+    margin-top: 20px;
+    text-align: left;
+  }
+
+  .left-lg-col {
+      background-color: #dfe4ea;
+  }
 </style>

7fb66107e4f78ddf6efce16e65b7d04ba096d2a3
Rob <robert.systemdeveloper@gmail.com>
2021-04-04 09:37:42 +0200

    Added hover effect logo

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,6 +1,11 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-secondary bg-secondary p-4">
-        <img id = "logo" src="src\assets\logo.png" width=50 height=50 class="img-fluid" alt="Responsive image">
+
+        <div class="img__wrap">
+            <img id = "logo" src="src\assets\logo.png" width=50 height=50 class="img-fluid" alt="Responsive image">
+            <p class="img__description">Fast predictions</p>
+        </div>
+
         <ul id=ul-items>
             <li class="nav-item">
                 <router-link to='/'>Make a prediction</router-link>
@@ -49,6 +54,30 @@ export default {
 
     #ul-items li {
         display: inline-block;
+    }
+
+    .img__wrap {
+        position: relative;
+        height: 50px;
+        width: 200px;
+    }
+
+    .img__description {
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 10;
+        right: 0;
+        background: secondary;
+        color: #fff;
+        visibility: hidden;
+        opacity: 0;
+        transition: opacity .2s, visibility .2s;
+    }
+
+    .img__wrap:hover .img__description {
+        visibility: visible;
+        opacity: 1;
     }
 
 </style>

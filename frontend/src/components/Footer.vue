dec6f1196b802e08f3d5879c7bc10e89bb67fdc5
Rob <robert.systemdeveloper@gmail.com>
2021-04-05 08:33:19 +0200

    Footer company name change

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,8 +1,8 @@
 <template>
     <footer class="container-fluid bg-secondary text-light p-3">
         <div class="row">
-            <div class="col-12 col-md-4 text-center">&copy; 2021 Fast predictor</div>
-            <div class="col-12 col-md-4 text-center">predictor@fastpredictor.com</div>
+            <div class="col-12 col-md-4 text-center">&copy; 2021 Fast predictions</div>
+            <div class="col-12 col-md-4 text-center">predictions@fastpredictions.com</div>
             <div class="col-12 col-md-4 text-center">010-123 456</div>
         </div>
   </footer>
